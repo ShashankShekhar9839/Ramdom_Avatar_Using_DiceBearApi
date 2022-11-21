aa3b4ca0d1417adb93caf20f5d20725fa4c8fbc6
ShashankShekhar9839 <111214162+ShashankShekhar9839@users.noreply.github.com>
2022-11-21 19:52:03 +0530

    project done

--- src/App.js
+++ src/App.js
@@ -1,24 +1,81 @@
-import logo from './logo.svg';
+import React from "react";
 import './App.css';
 
+
+// sprites copie from the dicebear api
+const sprites = [
+  "male",
+  "female",
+  "human",
+  "identicon",
+  "initials",
+  "bottts",
+  "avataaars",
+  "jdenticon",
+  "gridy",
+  "micah"
+];
+
 function App() {
+  const [imgURL, setImgURL] = React.useState("");
+  const [inputVal, setInputVal] = React.useState("");
+  const [selectedSprite, setSelectedSprite] = React.useState(sprites[0]);
+
+  const handleInputChange = (e) => {
+    setInputVal(() => {
+      if (e.target.value.length > 0) {
+        setImgURL(
+
+          // changing values in url on changing input
+          `https://avatars.dicebear.com/api/${selectedSprite}/${e.target.value}.svg`
+        );
+      }
+      return e.target.value;
+    });
+  };
+
+  const handleSpriteChange = (e) => {
+    setSelectedSprite(() => {
+      if (e.target.value.length > 0) {
+        setImgURL(
+
+          // changing url based on dropdown option
+          `https://avatars.dicebear.com/api/${e.target.value}/${inputVal}.svg`
+        );
+      }
+      return e.target.value;
+    });
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+          
+    
+    <div className="parent">
+
+   
+    <div className="container">
+          <img src={imgURL} alt='dicebear'
+          style={{height:'5rem',width:'5rem'}}
+          ></img>
+          <h4>Select Your Sprite</h4>
+
+         <input className='input' placeholder="Enter what you want" onChange={handleInputChange} ></input>
+          <select className="options"  onClick={handleSpriteChange }>
+            {
+             sprites.map((item)=>{
+              return(
+                <option  >{item}</option>
+              )
+             })
+            }
+          </select>
     </div>
+
+    </div>
+
+
+
+   
   );
 }
 
